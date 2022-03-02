982d470fca9831efd83c028ca3a61bcda1ff63ef
Mr.Beanie <69245646+LouieSuh@users.noreply.github.com>
2022-03-02 22:02:00 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function mainpage(){
+    return <h>Main Page Test</h>
+        
+    }
+
+
+    export default mainpage;
+
+    const Container = styled.div
+    height = flex;
+    backgroundColor = red;
+    display = "flex"
+
+    ;
