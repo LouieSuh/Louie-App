c1030af3ec7f9bd0509348b6806fed9624f3529e
Mr.Beanie <69245646+LouieSuh@users.noreply.github.com>
2022-03-18 08:53:24 +0900

    Update index.js

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,10 +1,14 @@
+import styled from "styled-components"
+import React, {useState} from "react"
+import {useHistory} from "react-router-dom"
+const Wrapper = styled
 function mainpage(){
     return <h>Main Page Test</h>
         
     }
 
 
-    export default mainpage;
+    export default Mainpage;
 
     const Container = styled.div
     height = flex;
@@ -12,3 +16,41 @@ function mainpage(){
     display = "flex"
 
     ;
+
+    const Left = styled.div
+    height = flex;
+    backgroundColor = white;
+    display = "flex"
+
+    ;
+
+    const Right = styled.div
+    height = flex;
+    backgroundColor = blue;
+    display = "fixed"
+
+    ;
+
+    function Main() {
+        return(
+            <Wrapper>
+                <Left>
+                    <h1>Hi, my name is Louie!</h1>
+                </Left>
+                <Right>
+                    <p> A student who is attending BCC since grade 6. 
+                        I am also good at playing sports 
+                        and likes to play games.  </p>
+                    <a href="https://github.com/2023LouieSuh">
+                    </a>
+                </Right>
+            </Wrapper>
+        );
+    }
+
+    export default Main();
+
+    
+
+
+
